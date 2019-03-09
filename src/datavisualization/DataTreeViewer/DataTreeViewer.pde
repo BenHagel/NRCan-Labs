15667bf9e566e5794d20d3f0114db5314a3422b5
Ben <ben.hagel@hotmail.com>
2019-03-09 15:51:31 -0500

    git ignore add

---
+++ src/datavisualization/DataTreeViewer/DataTreeViewer.pde
@@ -0,0 +1,11 @@
+String[] rawStrings;
+
+void setup(){
+  size(800, 600, P3D);
+  rawStrings = loadStrings("raw_web_data.txt");
+}
+
+void draw(){
+  
+  
+}
---
+++ src/webcrawler/pupeteer/index.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+const theUtil = require('util');
+
+const sleep = theUtil.promisify(setTimeout);
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('https://www.energystar.gov/products', {
+		'waitUntil': 'networkidle2',
+		'timeout': 10000
+	}).then(() => {
+		console.log('succ');
+	}).catch((res) => {
+		console.log('failed ' + res);
+	});
+	page.setViewport({
+		width: 1200,
+		height: 800
+	});
+
+	/*===================This would be for maintaining active sessions on a webpage
+	await page.type('#email', 'bhag@gmail.com');
+	await page.type('#pass', 'password123');
+	await page.click('#loginbutton');
+
+	await page.waitForNavigation();
+
+	await page.goto('https://www.messenger.com/t/523535423542345', {
+		'waitUntil': 'networkidle2'
+	});
+	*/
+
+	//console.log('first screen');
+
+	await page.screenshot({path: 'example3.png'});
+	await sleep(1000);
+	//page.keyboard.press('Enter');
+	await browser.close();
+})();
+
+console.log('start pupper');
